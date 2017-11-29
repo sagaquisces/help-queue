990a56da7e0df085d638212d1cc8d71298037833
Michael Dunlap <michaeldunlap@earthlink.net>
2017-11-29 10:51:03 -0800

    use babel and webpack to transpile and bundle. update README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # help-queue
+
+npm install react react-dom --save
+npm install webpack --save-dev
+(npm install webpack -g)
+npm install babel-core babel-loader babel-preset-env babel-preset-react --save-dev
---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+	<div>
+		<h1>Help Queue</h1>
+		<h3>G3</h3>
+		<h3>Devin and Devon</h3>
+		<p><em>Bootstrap won't fire up correctly</em></p>
+	</div>,
+	document.getElementById('react-app-root')
+);
+
+
