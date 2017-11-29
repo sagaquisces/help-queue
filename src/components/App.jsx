03dc9a72b817e8d5638497bd91d2f06062291fab
Michael Dunlap <michaeldunlap@earthlink.net>
2017-11-29 11:12:30 -0800

    create App component to house all child elements

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+			<h1>Help Queue</h1>
+			<h3>G3</h3>
+			<h3>Devin and Devon</h3>
+			<p><em>Bootstrap won't fire up correctly</em></p>
+	</div>
+	);
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App"
 
 ReactDOM.render(
-	<div>
-		<h1>Help Queue</h1>
-		<h3>G3</h3>
-		<h3>Devin and Devon</h3>
-		<p><em>Bootstrap won't fire up correctly</em></p>
-	</div>,
+	<App />,
 	document.getElementById('react-app-root')
 );
 
