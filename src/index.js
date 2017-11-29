b375543a516cae225a6297f977d00aa3b4e4e2e3
Michael Dunlap <michaeldunlap@earthlink.net>
2017-11-29 10:19:15 -0800

    wrap render in a function to update at setInterval.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-var heading = React.createElement('h1', {}, 'Help Queue');
-var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-var app = React.createElement('div', {}, heading, clock);
+function liveTime() {
+	var heading = React.createElement('h1', {}, 'Help Queue');
+	var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+	var app = React.createElement('div', {}, heading, clock);
 
 
-ReactDOM.render(
-	app,
-	document.getElementById('react-app-root')
-);
+	ReactDOM.render(
+		app,
+		document.getElementById('react-app-root')
+	);
+}
+
+setInterval(liveTime, 1000);
