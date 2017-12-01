acde3b26449249cf3d7afd51d3f73ac1f4d09e13
Michael Dunlap <michaeldunlap@earthlink.net>
2017-12-01 12:08:44 -0800

    use store to add values to state.

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -12,7 +12,7 @@ describe("Ticket list reducer", () => {
 			'Aimen'
 		],
 		location: "4b",
-		description: "Jest is being a diva and won't play nice with Webpack",
+		issue: "Jest is being a diva and won't play nice with Webpack",
 		timeOpened: 1500000000000,
 		id: 0
 	};
@@ -22,12 +22,12 @@ describe("Ticket list reducer", () => {
 	});
 
 	test('should add ticket to list array', () => {
-		const { names, location, description, timeOpened, id } = ticketInfo;
+		const { names, location, issue, timeOpened, id } = ticketInfo;
 		action = {
 			type: c.ADD_TICKET,
 			names: names,
 			location: location,
-			description: description,
+			issue: issue,
 			timeOpened: timeOpened,
 			id: id
 		};
--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Ticket from "../models/ticket.js";
 import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import c from './../constants';
 
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
@@ -12,12 +14,21 @@ class NewTicketForm extends React.Component {
 
     constructor(props){
         super(props);
-        console.log(props);
         this.handleNewTicketFormSubmission = this.handleNewTicketFormSubmission.bind(this);
     }
 
     handleNewTicketFormSubmission(e) {
         e.preventDefault();
+        const { dispatch } = this.props;
+        const action = {
+            type: c.ADD_TICKET,
+            id: null,
+            names: ReactDOM.findDOMNode(this._names).value,
+            location: ReactDOM.findDOMNode(this._location).value,
+            issue: ReactDOM.findDOMNode(this._issue).value,
+            timeOpened: new Date().getTime()
+        }
+        dispatch(action);
         var newTicket = new Ticket(ReactDOM.findDOMNode(this._names).value, ReactDOM.findDOMNode(this._location).value, ReactDOM.findDOMNode(this._issue).value);
         this.props.onNewTicketCreation(newTicket);
         this.props.hideFormAfterSubmission();
@@ -61,5 +72,7 @@ NewTicketForm.propTypes = {
     onNewTicketCreation: PropTypes.func,
     hideFormAfterSubmission : PropTypes.func
 };
+
+NewTicketForm = connect()(NewTicketForm);
 
 export default NewTicketForm;
